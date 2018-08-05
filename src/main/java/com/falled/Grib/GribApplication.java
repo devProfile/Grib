9af49492539f7d6632c0ae1ac2d82fbb3ca4d97b
Jura <jurakulek@gmail.com>
2018-08-05 19:09:16 +0300

    Create project and some controller things

---
+++ src/main/java/com/falled/Grib/GribApplication.java
@@ -0,0 +1,12 @@
+package com.falled.Grib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GribApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GribApplication.class, args);
+	}
+}
---
+++ src/test/java/com/falled/Grib/GribApplicationTests.java
@@ -0,0 +1,16 @@
+package com.falled.Grib;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GribApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
